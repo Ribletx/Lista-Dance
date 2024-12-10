1da952657e6708603d01eb226c5aa5fd72fa4040
Ribletx <cjsalgadod@gmail.com>
2024-12-10 03:09:05 -0300

    Commit inicial

---
+++ songlist/src/app/idiomas/en.js
@@ -0,0 +1,11 @@
+const en = {
+  mainTitle: "Welcome to main page",
+  mainGameTitle: "Welcome to the Game Hub",
+  mainTextTitle: "Welcome to the Story Hub",
+  welcomeMessage: "Welcome, {name}!",
+  signIn: "Sign In",
+  register: "Register",
+  logout: "Logout",
+};
+
+export default en;
---
+++ songlist/src/app/idiomas/es.js
@@ -0,0 +1,11 @@
+const es = {
+  mainTitle: "Bienvenido a la pagina principal",
+  mainGameTitle: "Bienvenido al Centro de Juegos",
+  mainTextTitle: "Bienvenido al Centro de Cuentos",
+  welcomeMessage: "Bienvenido, {name}!",
+  signIn: "Iniciar sesión",
+  register: "Registrarse",
+  logout: "Cerrar sesión",
+};
+
+export default es;
