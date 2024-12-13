c7de2ed289488321a7ce9a960f1c4bd436a2258d
Ribletx <cjsalgadod@gmail.com>
2024-12-12 21:38:41 -0300

    Sin apartado de comentarios

--- songlist/src/app/components/header.js
+++ songlist/src/app/components/header.js
@@ -62,13 +62,13 @@ export default function Header() {
       </h1>
 
       {/* Botón para agregar nueva canción como círculo */}
-      <div className="absolute bottom-4 right-4">
+      {/*<div className="absolute bottom-4 right-4">
         <Link href="/nueva">
           <button className="w-12 h-12 bg-purple-800 text-white rounded-full flex items-center justify-center text-3xl hover:bg-purple-900 transition-all duration-300">
             +
           </button>
         </Link>
-      </div>
+      </div>*/}
 
     </header>
   );
